0ac8438ba977a45bc14e3a25e4303af201e9b6be
Felipe Moura <f.santosdemoura0703@gmail.com>
2024-05-08 22:24:47 -0300

    Adicionando as pastas ao repositorio

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const db = require('./db/connection');
+
+const PORT = 3000;
+
+app.listen(PORT, function(){
+    console.log(`O express está rodando na porta ${PORT} `);
+})
+
+// db connections
+db
+    .authenticate()
+    .then(()=>{
+      console.log("Conectou ao banco com sucesso");   
+    })
+     .catch(err => {
+        console.log("Ocorreu um erro ao conectar")
+    });
+
+
+// routes
+app.get('/',(req, res) => {
+    res.send("ESTA FUNCIONANDO 2")
+})
